4dd53d5373810cec0053f2c0b1470545d560fdf5
Duy Huynh <dhuynh88@uw.edu>
2015-11-15 23:56:18 -0800

    minimax

--- Assignment1-TicTacToe/tictactoeAgent.js
+++ Assignment1-TicTacToe/tictactoeAgent.js
@@ -50,114 +50,38 @@ Agent.prototype.selectMove = function(board) {
 		-------------
 	*/
 
-	var move; // The selected move
+	// (array of possible moves, initial depth, maximizing player?)
+	return this.minimax(board, 0, true);
 
-	// Array of moves that are available
-	var freeCells = []; // Array of free cells
-	for (var i = 1; i < 10; i++) {
-		if (board.cellFree(i)) freeCells.push(i);
-	}
-
-	// Always pick the middle if available.
-	if (this.takeMiddle(freeCells)) {
-		move = 5;
-	}
-
-	// If middle was unavailable, then block or win
-	if (move === undefined) {
-		var xPairs = this.getPairs(board.X)
-		var oPairs = this.getPairs(board.O)
-
-		// I went first! I'm X?
-		move = board.X.length === board.O.length ? this.blockOrWin(xPairs, oPairs, freeCells) : this.blockOrWin(oPairs, xPairs, freeCells);
-	}
-
-	// Take a corner if no blocking/winning needed
-	if (move === undefined) {
-		move = this.takeCorner(freeCells);
-	}
-
-	// If at this point there is no obivious move, take a corner
-	if (move === undefined) {
-		move = freeCells[Math.floor(Math.random() * freeCells.length)];
-	}
-
-	// console.log("X: " + board.X)
-	// console.log("O: " + board.O)
-	return move;
-
-}
-
-// Grab an empty corner.
-Agent.prototype.takeCorner = function(available) {
-	var self = this;
-	var corners = [2, 4, 6, 8];
-	var availableCorners = available.filter(function(n) {
-		return corners.indexOf(n) != -1;
-	});
-	// console.log("Corners available: " + availableCorners)
-	return availableCorners[Math.floor(Math.random() * availableCorners.length)];
-}
-
-// Always shoot for the middle.
-Agent.prototype.takeMiddle = function(available) {
-	return available.indexOf(5) != -1;
-}
-
-// Returns an array of unique pairs given an array of elements
-Agent.prototype.getPairs = function(array) {
-	var pairs = [];
-	for (var i = 0; i < array.length - 1; i++) {
-		for (var j = i + 1; j < array.length; j++) {
-			pairs.push([array[i], array[j]]);
-		}
-	}
-	return pairs;
-}
-
-// Attempt to win if possible, otherwise block opponent
-Agent.prototype.blockOrWin = function(myPairs, theirPairs, available) {
-	var self = this;
-	var returnValue = this.twoInRow(myPairs, available);
-	if (returnValue === undefined) {
-		return self.twoInRow(theirPairs, available);
-	}
-	return returnValue;
-}
-
-// Detect two-in-a-rows for potential wins or blocks.
-Agent.prototype.twoInRow = function(pairs, available) {
-	var self = this;
-	var returnValue;
-	pairs.forEach(function(pair) {
-		var value = 15 - (pair[0] + pair[1]);
-		if (available.indexOf(value) != -1) {
-			returnValue = value;
-		}
-	})
-	return returnValue
 }
 
 // Attempt at Minimax below...
 
-Agent.prototype.result = function(board) {
+Agent.prototype.score = function(board, depth) {
 	if (board.gameOver === 1) {
-		return 1; // player won
+		return 10 - depth; // player won
 	} else if (board.gameOver === 2) {
-		return -1; // opponent won
+		return depth - 10; // opponent won
 	} else if (board.gameOver === 3) {
 		return 0; // a draw
-	}
+	} else return 0; // Not end game yet
 }
 
 
-Agent.prototype.minimax = function(board) {
+Agent.prototype.minimax = function(board, depth, maximizingPlayer) {
+
+
+	var freeCells = [];
+	for (var i = 1; i < 10; i++) {
+		if (board.cellFree(i)) freeCells.push(i);
+	}
+	console.log(freeCells);
 
 	var that = this;
 	// Base case: A game-over, return minimax value
 	// Win: 1, Loss: -1, Draw: 0.
 	if (board.gameOver === 0) {
-		return this.result(board);
+		return this.score(board, depth);
 	}
 
 
@@ -168,26 +92,24 @@ Agent.prototype.minimax = function(board) {
 	var newBoard = board;
 	newBoard.clone;
 	// For each available move, run minimax recursively
-	this.getFreeCells(board).forEach(function(cell) {
+	freeCells.forEach(function(cell) {
 		// Clone then seed board with a possible move
 		newBoard.move(cell);
 		// Push the outcome to a list of results
-		results.push(that.minimax(newBoard));
+		results.push(that.minimax(newBoard, depth + 1, true ));
 		// Track the moves in array
 		moves.push(cell);
 	})
 
-	// Find max or min depending on turn order
-
 	// Player X was first, so get MAXIMIZED score
-	if (newBoard.X.length === newBoard.O.length) {
-		var maxScoreIndex = results.indexOf(Math.max.apply(Math, results));
+	if (maximizingPlayer) {
+		var maxScoreIndex = results.indexOf(Math.max.apply(null, results));
 		move = moves[maxScoreIndex];
 		console.log("MAX MOVE: " + move)
 		return results[maxScoreIndex];
 	} else {
 		// Player O goes second, so MINIMIZED score
-		var minScoreIndex = results.indexOf(Math.min.apply(Math, results));
+		var minScoreIndex = results.indexOf(Math.min.apply(null, results));
 		move = moves[minScoreIndex];
 		console.log("MIN MOVE: " + move)
 		return results[minScoreIndex];
